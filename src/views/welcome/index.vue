853d31879fdbc36b8fed354305315e08c3807236
刘胜彪 <1751097756@qq.com>
2020-07-15 16:07:26 +0800

    eslint文件配置

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -8,9 +8,9 @@ export default {
   data() {
     return {}
   },
-  //生命周期 - 创建完成（访问当前this实例）
+  // 生命周期 - 创建完成（访问当前this实例）
   created() {},
-  //生命周期 - 挂载完成（访问DOM元素）
+  // 生命周期 - 挂载完成（访问DOM元素）
   mounted() {}
 }
 </script>
